90599d3aeb1f596f396ffbfa1925c4e2736bc794
eschoyeztomas <eschoyeztomas@gmail.com>
2022-10-13 11:18:19 -0300

    .

---
+++ PatrónUoW/UnitOfWork/UnitOfWork/UnidadDeTrabajo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitOfWork
+{
+    internal class UnidadDeTrabajo
+    {
+        List<cBase> objetosNuevos;
+        List<cBase> objetosModificados;
+        List<cBase> objetosEliminados;
+        
+        public UnidadDeTrabajo()
+        {
+            objetosNuevos = new List<cBase>();
+            objetosModificados = new List<cBase>();
+            objetosEliminados = new List<cBase>();
+        }
+        
+        public void Añadir(cBase newObj)
+        {
+
+        }
+        
+        public void Eliminar(cBase delObj)
+        {
+        
+        }
+        
+        public void Modificar(cBase modObj)
+        {
+        
+        }
+        
+        public void Confirmar()
+        {
+        
+        }
+        
+        private void Limpiar()
+        {
+            objetosNuevos.Clear();
+            objetosModificados.Clear();
+            objetosEliminados.Clear();
+        }
+    }
+}
+
+
---
+++ PatrónUoW/UnitOfWork/UnitOfWork/cBase.cs
@@ -0,0 +1,6 @@
+﻿namespace UnitOfWork
+{
+    internal class cBase
+    {
+    }
+}
